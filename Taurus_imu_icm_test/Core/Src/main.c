1c42fd3b00797859548ccd2f5970a0a0d76406b1
F1Lse <1776700764@qq.com>
2025-02-24 13:48:47 +0800

    轮腿测试

--- Taurus_imu_icm_test/Core/Src/main.c
+++ Taurus_imu_icm_test/Core/Src/main.c
@@ -265,8 +265,8 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 	if(htim->Instance==TIM3)
 	{
 	dt_can = DWT_GetDeltaT(&dwt_count);
-	can_std_transmit(&hfdcan1,0x001,imu_msg_send.pit_msg.array);
-	can_std_transmit(&hfdcan1,0x002,imu_msg_send.yaw_msg.array);
+	can_std_transmit(&hfdcan1,0x011,imu_msg_send.pit_msg.array);
+	can_std_transmit(&hfdcan1,0x012,imu_msg_send.yaw_msg.array);
 //	can_std_transmit(&hfdcan1,0x003,imu_msg_send.rol_msg.array);	
 	}
   /* USER CODE END Callback 1 */
